6d3895387daed727a4e3378933ea943584930051
alcarinquenessima <106439733+alcarinquenessima@users.noreply.github.com>
2024-09-09 12:02:18 +0300

    Update TestBase.java
    deleting spare imports

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,15 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attachments;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.util.Map;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
